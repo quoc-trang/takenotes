88446ee9b0687ff160b2b3f592173cfb9b8587cd
quoctrang <quoctrang2701@gmail.com>
2025-07-11 20:05:59 +0700

    Update authentication middleware to prevent execution on the server side, ensuring proper handling of user authentication in client-side navigation.

--- frontend/middleware/auth.ts
+++ frontend/middleware/auth.ts
@@ -1,4 +1,6 @@
 export default defineNuxtRouteMiddleware(() => {
+  if(import.meta.server) return
+  
   const authStore = useAuthStore()  
   if (!authStore.isLoggedIn) {
     return navigateTo('/login')
